e2998eaf78c9b1e0574cf9f45c8b9539aa3f6439
Cory0726 <lkfu520@gmail.com>
2025-11-03 20:14:54 +0800

    update

--- src/main.py
+++ src/main.py
@@ -7,4 +7,4 @@ from src.basler_tof_cam_grab import pcl_to_rawdepth
 if __name__ == '__main__':
     basler_tof_cam_grab.grab_one_point_cloud()
     img = pcl_to_rawdepth(basler_tof_cam_grab.grab_one_point_cloud())
-    cv2.imwrite("test", img)
+    cv2.imwrite("test.png", img)
