f359df4662aa5a5ae2149f296792d500ff7ac7c5
Cory0726 <lkfu520@gmail.com>
2025-11-03 15:38:01 +0800

    update

---
+++ src/basler_cam_init.py
@@ -0,0 +1,32 @@
+from pypylon import pylon
+
+def list_basler_devices() -> None:
+    """ 
+    List all devices which connected to the computer.
+    """
+    # Build the transport layer factory
+    tl_factory = pylon.TlFactory.GetInstance()
+    # List the devices
+    devices = tl_factory.EnumerateDevices()
+    for i, dec in enumerate(devices):
+        print(f"[{i}] {dec.GetModelName()} - {dec.GetDeviceClass()}"
+            + f"- {dec.GetFullName()} - {dec.GetSerialNumber()}")
+
+def create_basler_cam(serial_number: str) -> pylon.InstantCamera:
+    """
+    Create a Basler camera instance by serial number.
+
+    Args:
+        serial_number (str): Basler camera's serial number.
+
+    Returns:
+        pylon.InstantCamera: Basler camera instance
+    """
+    # Get the transport layer factory
+    tl_factory = pylon.TlFactory.GetInstance()
+    # Set the device information
+    device = pylon.DeviceInfo()
+    device.SetSerialNumber(serial_number)
+    # Create the camera
+    cam = pylon.InstantCamera(tl_factory.CreateDevice(device))
+    return cam
---
+++ src/main.py
@@ -0,0 +1,4 @@
+import basler_rgb_cam_grab
+
+if __name__ == '__main__':
+    basler_rgb_cam_grab.stream_rgb_img()
